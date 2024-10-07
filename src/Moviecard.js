3c53c31f934cc4da065d01ba7072d44d0d727ac3
Mayur <madankarmayur5@gmail.com>
2024-10-07 12:15:03 +0530

    class to functional components

--- src/Moviecard.js
+++ src/Moviecard.js
@@ -1,71 +1,68 @@
 import { Component } from "react";
 
-class MovieCard extends Component {
-  render() {
-    const { movies, addStars, removeStars, favToggle, cartToggle } = this.props;
-    const { title, plot, price, poster, rating, star, fav, cart } =
-      this.props.movies;
+function MovieCard() {
+  const { movies, addStars, removeStars, favToggle, cartToggle } = props;
+  const { title, plot, price, poster, rating, star, fav, cart } = props.movies;
 
-    return (
-      <div className="main">
-        <div className="movie-card">
-          <div className="left">
-            <img alt="Poster" src={poster} />
-          </div>
-          <div className="right">
-            <div className="title">{title}</div>
-            <div className="plot">{plot}</div>
-            <div className="price">{price}</div>
-            <div className="footer">
-              <div className="rating">{rating}</div>
-              <div className="star-dis">
-                <img
-                  className="str-btn"
-                  alt="decrease"
-                  src="https://cdn-icons-png.flaticon.com/128/10905/10905142.png"
-                  onClick={() => {
-                    removeStars(movies);
-                  }}
-                />
-                <img
-                  alt="star"
-                  src="https://cdn-icons-png.flaticon.com/128/1828/1828884.png"
-                  className="stars"
-                />
-                <img
-                  alt="increase"
-                  className="str-btn"
-                  src="https://cdn-icons-png.flaticon.com/128/748/748113.png"
-                  onClick={() => {
-                    addStars(movies);
-                  }}
-                />
-                <span className="starCount">{star}</span>
-              </div>
-
-              <button
-                className={fav ? "unfavourite-btn" : "favourite-btn"}
+  return (
+    <div className="main">
+      <div className="movie-card">
+        <div className="left">
+          <img alt="Poster" src={poster} />
+        </div>
+        <div className="right">
+          <div className="title">{title}</div>
+          <div className="plot">{plot}</div>
+          <div className="price">{price}</div>
+          <div className="footer">
+            <div className="rating">{rating}</div>
+            <div className="star-dis">
+              <img
+                className="str-btn"
+                alt="decrease"
+                src="https://cdn-icons-png.flaticon.com/128/10905/10905142.png"
                 onClick={() => {
-                  favToggle(movies);
+                  removeStars(movies);
                 }}
-              >
-                {fav ? "Un-Favourite" : "Favourite"}
-              </button>
-
-              <button
-                className={cart ? "remove-btn" : "cart-btn"}
+              />
+              <img
+                alt="star"
+                src="https://cdn-icons-png.flaticon.com/128/1828/1828884.png"
+                className="stars"
+              />
+              <img
+                alt="increase"
+                className="str-btn"
+                src="https://cdn-icons-png.flaticon.com/128/748/748113.png"
                 onClick={() => {
-                  cartToggle(movies);
+                  addStars(movies);
                 }}
-              >
-                {cart ? "Remove from cart" : "Add to cart"}
-              </button>
+              />
+              <span className="starCount">{star}</span>
             </div>
+
+            <button
+              className={fav ? "unfavourite-btn" : "favourite-btn"}
+              onClick={() => {
+                favToggle(movies);
+              }}
+            >
+              {fav ? "Un-Favourite" : "Favourite"}
+            </button>
+
+            <button
+              className={cart ? "remove-btn" : "cart-btn"}
+              onClick={() => {
+                cartToggle(movies);
+              }}
+            >
+              {cart ? "Remove from cart" : "Add to cart"}
+            </button>
           </div>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default MovieCard;
--- src/navbar.js
+++ src/navbar.js
@@ -1,23 +1,21 @@
 import { Component } from "react";
 import styles from "./nav.module.css";
-class Navbar extends Component {
-  render() {
-    const { cartCount } = this.props;
-    return (
-      <>
-        <div className={styles.nav}>
-          <div className={styles.title}>Movie App</div>
-          <div className={styles.container}>
-            <img
-              alt="cart icon"
-              src="https://cdn-icons-png.flaticon.com/128/3171/3171927.png"
-              className={styles.image}
-            />
-            <span className={styles.count}>{cartCount}</span>
-          </div>
+function Navbar() {
+  const { cartCount } = this.props;
+  return (
+    <>
+      <div className={styles.nav}>
+        <div className={styles.title}>Movie App</div>
+        <div className={styles.container}>
+          <img
+            alt="cart icon"
+            src="https://cdn-icons-png.flaticon.com/128/3171/3171927.png"
+            className={styles.image}
+          />
+          <span className={styles.count}>{cartCount}</span>
         </div>
-      </>
-    );
-  }
+      </div>
+    </>
+  );
 }
 export default Navbar;
